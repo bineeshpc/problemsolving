61086bf514f067d666330bf5a20c4fd4c036ec61
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2015-05-29 17:09:55 +0530

    Working code. Made the code inside an object.

--- trierep.py
+++ trierep.py
@@ -1,25 +1,33 @@
+
+
+class Trie:
+
+    def __init__(self, trie):
+        self.trie = trie
+
+    def stringrep(self, d):
+        try:
+            keylist = d.keys()
+            keylist.sort()
+        except AttributeError:
+            return '{}'.format(d)
+        s = '{'
+        for num, key in enumerate(keylist):
+            if isinstance(key, basestring):
+                strrep = "'{}'".format(key)
+            else:
+                strrep = str(key)
+            s += strrep + ': ' + self.stringrep(d[key])
+            if num != len(keylist) - 1:
+                s += ', '
+        s += '}'
+        return s
+
+
 a = {'a': {'c': {'a': (None, 'aca')}, 'b': {'c': (None, 'abc'), 'd': (None, 'abd')}}, None: {'b': {'d': (None, 'Nbd')}}}
-
-def stringrep(d):
-    try:
-        keylist = d.keys()
-        keylist.sort()
-    except AttributeError:
-        return '{}'.format(d)
-    s = '{'
-    for num, key in enumerate(keylist):
-        if isinstance(key, basestring):
-            strrep = "'{}'".format(key)
-        else:
-            strrep = str(key)
-        s += strrep + ': ' + stringrep(d[key])
-        if num != len(keylist) - 1:
-            s += ', '
-    s += '}'
-    return s
-
 print a
-x = stringrep(a)
+t = Trie(a)
+x = t.stringrep(a)
 print x
 sorted_a = eval('dict({})'.format(x))
 print a == sorted_a
@@ -27,5 +35,6 @@ print a == sorted_a
 b = {'a': {'b': {'c': (None, 'abc'), 'd': (None, 'abd')}, 'c': {'a': (None, 'aca')}}, None: {'b': {'d': (None, 'Nbd')}}}
 print b == sorted_a
 
-y = stringrep(b)
+t1 = Trie(b)
+y = t1.stringrep(b)
 print x == y
