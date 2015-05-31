9d64d19ceb2ff6656e9b5ee68f69a4961712d441
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2015-05-31 19:47:48 +0530

    Added connect.py used for connecting to wifi and mountall.py for mounting all partitions

---
+++ bin/connect.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python2
+
+import commands
+import os
+import time
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--reversed", help="mount all partitions",
+                                        action="store_true")
+parser.add_argument("-c", "--choice", help="choose between the choices displayed",
+                    action="store_true")
+args = parser.parse_args()
+
+def getwifinames():
+    wifinames_cmd = "connmanctl scan wifi && connmanctl services|grep -P 'loaddown|HomeNearPark|LoadDown|Furious'"
+    wifis = {}
+    for line in commands.getoutput(wifinames_cmd).split('\n')[1:]:
+        try:
+            ssidname, ssid = line.split()[-2:]
+            wifis[ssidname] =  ssid
+        except ValueError:
+            #line did not have enough values to unpack
+            pass
+    return wifis
+
+#getwifinames()
+
+def connect(reversed=False, choice=False):
+    preferred_order = ['LoadDown2', 'loaddown1', '8', 'loaddown3', 'HomeNearPark1']
+    if reversed:
+        preferred_order = preferred_order[::-1]
+    if choice:
+        for n, v in enumerate(preferred_order):
+            print n, v 
+        num = int(raw_input("Enter choice: "))
+        preferred_order = [preferred_order[num]]
+    wifis = getwifinames()
+    #print wifis
+    for wifi in preferred_order:
+        try:
+            print "Trying to connect to", wifi
+            connect_cmd = 'connmanctl connect {} && connmanctl state;ping -c 4 8.8.8.8;ping -c 4 google.com'.format(wifis[wifi])
+            if commands.getstatusoutput(connect_cmd)[0] == 0:
+                print "connected to", wifi
+                return True
+        except KeyError:
+            print wifi, "is not broadcasting now"
+            
+    return False
+            
+
+
+if args.choice:
+    connect(args.reversed, args.choice)
+while True:
+    if commands.getstatusoutput('ping -c 4 google.com')[0] != 0:
+        connected = connect(args.reversed)
+        if connected:
+            time.sleep(15)
+"""    
+connmanctl scan wifi
+connmanctl services
+connmanctl connect wifi_9439e53b957f_486f6d654e6561725061726b31_managed_psk
+connmanctl state
+ip addr
+ping -c 4 8.8.8.8
+ping -c 4 google.com
+"""
---
+++ bin/mountall.py
@@ -0,0 +1,63 @@
+#! /bin/python
+import subprocess
+import re
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--mount", help="mount all partitions",
+                    action="store_true")
+parser.add_argument("-u", "--umount", help="unmount all partitions",
+                    action="store_true")
+args = parser.parse_args()
+
+#proc = subprocess.Popen(['lsblk', '-f'], stdout=subprocess.PIPE)
+
+#print(proc.communicate()[0])
+
+blocks = subprocess.check_output(['lsblk', '-f'])
+
+#print(type(blocks))
+#print(blocks.decode("utf-8"))
+#for line in blocks.split('\n'):
+#    print(line.rstrip('\n'))
+
+def get_device_directory_mapping():
+    devices = []
+    for line in blocks.decode("utf-8").split('\n'):
+        oneline = line.split()
+#        print(oneline)
+        try:
+            if oneline[1] == 'swap': continue
+            devices.append((re.search("(?P<device>sda[0-9]*)", oneline[0]).groupdict()['device'], oneline[2]))
+        except (IndexError, AttributeError):
+            pass
+
+    #print(devices)
+    for a, b in devices:
+        b = b.lower()
+        a, b = "/dev/{}".format(a), "/mnt/{}".format(b)
+        yield a, b
+
+def mount(device, directory):
+    if subprocess.getstatusoutput('grep {} /proc/mounts'.format(device))[0] == 1:
+        print("mounting {} in {}".format(device, directory))
+        subprocess.call("sudo mount {} {}".format(device, directory), shell=True)
+    else:
+        print('{} is already mounted'.format(device))
+
+def umount(directory):
+    try:
+        grepout = subprocess.getstatusoutput(
+    'grep {} /proc/mounts'.format(directory))[0] 
+    except IndexError:
+        print("{} is not mounted".format(directory))
+        return 1
+    if grepout == 0:
+        print("unmounting {}".format(directory))
+        subprocess.call("sudo umount {}".format(directory), shell=True)
+
+for device, directory in get_device_directory_mapping():
+ #   print(device, directory)
+    if args.mount:
+        mount(device, directory)
+    if args.umount:
+        umount(directory)
