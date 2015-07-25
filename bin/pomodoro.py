8570d8d727a9f8e6e2da2e141ee5c50f1d90be67
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2015-07-25 06:44:18 +0530

    In python 3 division returns a float. typecasting it to int is required.

--- bin/pomodoro.py
+++ bin/pomodoro.py
@@ -6,7 +6,7 @@ interval = 25
 waittime = interval * 60
 waittime_mins = waittime / 60
 workinghours = 10000
-numiters = workinghours * 60 / interval
+numiters = int(workinghours * 60 / interval)
 for i in range(numiters):
     time.sleep(waittime)
     easygui.msgbox('Time up {}.'.format(waittime_mins), 'Help Poor Bini')
