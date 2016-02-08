12196a5de7c9aabb99ae3991ab6465bca9218642
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2016-02-08 11:43:04 +0530

    added comments to sqrt function

--- algs/python/sqrt.py
+++ algs/python/sqrt.py
@@ -1,11 +1,28 @@
+import random
+
 def sqrt(num):
+    """
+    Given a number returns its square root.
+    In this method we iteratively improve our guess.
+    As a result of it we get to the solution.
+    The guess converges to the solution.
+    We use a very small number epsilon.
+    
+    Args
+    num (float or integer): A float or integer.
+    Returns
+    the square root of the number
+    """
+    epsilon = 10**-7
     guess = float(num) / 2
-    while abs(guess * guess - num) > .0000001:
+    while abs(guess * guess - num) > epsilon:
         print(guess)
         q = num / guess
         guess = (q + guess) / 2
+    print("The square root of {} is {}".format(num, guess))
     return guess
 
 print(sqrt(2))
 print(sqrt(10))
 print(sqrt(25))
+print(sqrt(101.19))
