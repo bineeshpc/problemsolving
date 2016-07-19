7e807c1850984a32ceb0820c01b5384b51729dc6
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2016-07-19 14:16:38 +0530

    added a new puzzle leap year and days of month

---
+++ algs/python/puzzle_366_29_30_31.py
@@ -0,0 +1,12 @@
+"""
+29 * x + 30 * y + 31 * z = 366
+"""
+from itertools import permutations as p
+
+def is_366(x, y, z):
+    return 29 * x + 30 * y + 31 * z == 366
+    
+for x, y, z in p(range(0, 12), 3):
+    is_sol = is_366(x, y, z)
+    if is_sol:
+        print x, y, z
