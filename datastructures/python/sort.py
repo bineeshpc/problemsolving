f121ad1e1f2b18cf42c1e12b0b6960f34927531a
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2017-11-10 11:09:03 +0530

    added comments to stack

--- datastructures/python/sort.py
+++ datastructures/python/sort.py
@@ -12,36 +12,46 @@ def quicksort_haskell_style(arr):
             + [arr[0]] \
             + quicksort_haskell_style(greater)
 
+
 def partition(a, p, r):
-    x = a[r-1]
+    x = a[r-1]  # x is the last element in the array
     i = p - 1
     j = p
-    while j < r - 1:
+    while j < r - 1:  # j values from p to r - 1
+        print i, j, a, x
         if a[j] < x:
             i = i + 1
-            a[i], a[j] = a[j], a[i]
+            a[i], a[j] = a[j], a[i]  # swap a[i] and a[j]
         j += 1
-    a[i+1], a[r-1] = a[r-1], a[i+1]
+        print i, j, a, x
+    a[i+1], a[r-1] = a[r-1], a[i+1]  # swap a[i+1] and a[r-1]
     return i + 1
+
 
 def quicksorthelper(a, p, r):
     if p < r:
         q = partition(a, p, r)
+        print q, a
         quicksorthelper(a, p, q)
         quicksorthelper(a, q+1, r)
 
-def quicksort(a):
-    quicksorthelper(a, 0, len(a))
+
+def quicksort(arr):
+    """ Uses quicksort algorithm to sort an array """
+    quicksorthelper(arr, 0, len(arr))
+
 
 def mergesort(a):
     return mergesorthelper(a, 0, len(a))
-import time
+
+
 def mergesorthelper(a, begin, end):
     if begin < end:
         x = (begin + end) / 2
         mergesorthelper(a, begin, x)
-        mergesorthelper(a, x+1, end )
+        mergesorthelper(a, x+1, end)
         merge(a, begin, x, end)
+
 
 def merge(a, p, q, r):
     ai = p
@@ -54,16 +64,19 @@ def merge(a, p, q, r):
     while i < lenb and j < lenc:
         if b[i] < c[j]:
             a[ai] = b[i]
-            i+=1
+            i += 1
         else:
             a[ai] = c[j]
-            j+=1
+            j += 1
         ai += 1
     while i < lenb:
         a[ai] = b[i]
-        i+=1
-        ai +=1
+        i += 1
+        ai += 1
     while j < lenc:
         a[ai] = c[j]
-        j+=1
-        ai +=1        
+        j += 1
+        ai += 1
+
+
+quicksort([5, 4, 1, 2, 7, 6, 3])
--- datastructures/python/stack.py
+++ datastructures/python/stack.py
@@ -1,18 +1,30 @@
-
 class Stack:
     def __init__(self):
+        """ Initializes stack content to empty list """
         self.data = []
         self.size = 0
 
     def push(self, value):
+        """ Add an element to stack """
         self.data.append(value)
         self.size += 1
 
     def pop(self):
-        self.size -= 1
-        data = self.data[self.size]
-        del self.data[self.size]
-        return data
+        """ Remove and return the element from the top of the stack """
+        if self.size > 1:
+            self.size -= 1
+            data = self.data[self.size]
+            del self.data[self.size]
+            return data
+        else:
+            return None
+
+    def peek(self):
+        """ Returns the top element of the stack but does not delete it """
+        if self.size > 0:
+            return self.data[self.size - 1]
+        else:
+            return None
 
     def __str__(self):
         return ','.join([str(i) for i in self.data])
