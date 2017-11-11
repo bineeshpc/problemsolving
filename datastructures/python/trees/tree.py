e36a013890905f673d89d201af91010c2ba56f6b
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2017-11-11 23:55:38 +0530

    added a findmax methond in tree

--- datastructures/python/trees/tree.py
+++ datastructures/python/trees/tree.py
@@ -19,7 +19,7 @@ class BinaryTree:
 
     def insertleft(self, node):
         newnode = BinaryTree(node)
-        if self.left == None:
+        if self.left is None:
             self.left = newnode
         else:
             tmp = self.left
@@ -28,7 +28,7 @@ class BinaryTree:
 
     def insertright(self, node):
         newnode = BinaryTree(node)
-        if self.right == None:
+        if self.right is None:
             self.right = newnode
         else:
             tmp = self.right
@@ -36,9 +36,30 @@ class BinaryTree:
             newnode.right = tmp
             self.right = newnode
 
+    def find_max(self, max1=None):
+        if self.key:
+            if not max1:
+                max1 = self.key
+            max1 = max(self.key, max1)
+            if self.left:
+                leftmax = self.left.find_max(max1)
+                max1 = max(leftmax, max1)
+            if self.right:
+                rightmax = self.right.find_max(max1)
+                max1 = max(rightmax, max1)
+            return max1
+
+
 if __name__ == '__main__':
     bt = BinaryTree("/")
     bt.insertleft("sys")
     bt.insertright("tmp")
     print bt.getrootval(), bt.getleft().key
     print bt.getright().getrootval()
+
+    bt1 = BinaryTree(1)
+    bt1.insertleft(2)
+    bt1.insertright(3)
+    bt1.insertright(4)
+    
+    print bt1.find_max()
