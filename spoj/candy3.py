e881e2e2020e8c37748ab029c2002e28f5cf2f09
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2017-11-28 10:19:34 +0530

    solved ones so far, all working

--- spoj/candy3.py
+++ spoj/candy3.py
@@ -1,39 +1,23 @@
 # http://www.spoj.com/problems/CANDY3/
 
-def get_answer(all_candies, length1):
-    sum1 = 0
-    for i in all_candies:
-        x = int(i / length1)
-        print(x)
-        sum1 += i
-    return (sum1 % length1) == 0
-
 
 def process():
     num_cases = int(input())
     count = 0
-    all_candies = []
-    length1 = 0
-    eof = False
-    while(count < num_cases+1):
-        try:
-            candies = input()
-        except EOFError:
-            eof = True
-        if candies == '' or eof:
-            count += 1
-            #  print(all_candies, length1)
-            if length1 != 0:
-                if get_answer(all_candies, length1):
-                    print("YES")
-                else:
-                    print("NO")
-                all_candies = []
-                length1 = 0
+    
+
+    while count < num_cases:
+        blank = input()
+        num_students = int(input())
+        count_students = 0
+        sum1 = 0
+        while count_students < num_students:
+            sum1 += int(input())
+            count_students += 1
+        if sum1 % count_students == 0:
+            print("YES")
         else:
-            num_candies = int(candies)
-            all_candies.append(num_candies)
-            length1 += 1
-        
+            print("NO")
+        count += 1
 
 process()
--- spoj/infix_to_postfix.py
+++ spoj/infix_to_postfix.py
@@ -50,96 +50,31 @@ class Stack(object):
 def infix_to_postfix(expression):
     """ Given an expression in infix form return 
     it's postfix expression
-    Transform the algebraic expression with brackets 
-    into RPN form (Reverse Polish Notation). 
-    Two-argument operators: +, -, *, /, ^ 
-    (priority from the lowest to the highest),
-    brackets ( ). 
-    Operands: only letters: a,b,...,z. 
-    Assume that there is only one RPN form 
-    (no expressions like a*b*c). 
-
-    we need an operator stack
-    we need an operand stack
-    consider operator precedance
-    parathesis will have hightest precedance
-    paranthesis can probably be pushed into operand stack
-
-    +, -, *, /, ^  (operator precedance from low to high)
-    Consider case 1: a+b*c
     
-    we get a
-    it is an operand
-    output it
-    
-    we get +
-    it is an operator
-    since stack is empty
-    push to operator stack
-    
-    we get b
-    it is an operand
-    output it
-
-    we get * 
-    it is an operator
-    peek into stack
-    stack contains + on top.
-    push * to the stack
-    
-    now the expression is complete.
-    we see end of it.
-    pos all elements from operator stack and output it
-        
-    Consider case2: (a+b)*c
-
-    ( since it is bracket push to operator stack
-
-    a operand output it
-    + operator push to stack
-    b operand output it
-    ) pop everything until i find ) and output it
-    * since stack is empty push it
-    c operand output it
-    
-    pop everything from stack and output it
     """
     postfix = []
-    print expression
-    length_of_expression = len(expression)
-    stack = Stack(length_of_expression)
-    for letter in expression:
-        print letter
-        print "postfix", postfix
-        print stack
-        if is_operand(letter):
-            postfix.append(letter)
-        else:
-            if stack.is_empty():
-                stack.push(letter)
-                continue
-            top = stack.peek()
-            if get_precedance(letter) > get_precedance(top):
-                stack.push(letter)
-            else:
-                # pop everything from stack until the precedance of operator
-                # from stack is less than or equal to precedance of current operators
-                print "letter", letter
-                while get_precedance(stack.peek()) > get_precedance(letter):
-                    top = stack.pop()
-                    postfix.append(top)
-        if letter == '(':
-            stack.push(letter)
-        elif letter == ')':
+    stack_size = len(expression)
+    stack = Stack(stack_size)
+    for c in expression:
+        if c == '(':
+            stack.push(c)
+        elif c == ')':
             while stack.peek() != '(':
-                top = stack.pop()
-                postfix.append(top)
+                top_element = stack.pop()
+                postfix.append(top_element)
             stack.pop()  # pop out the '(' character
-
+        elif is_operand(c):
+            postfix.append(c)
+        elif is_operator(c):
+            while stack.peek() != '(' and get_precedance(stack.peek()) >= get_precedance(c):
+                top_element = stack.pop()
+                postfix.append(top_element)
+            stack.push(c)
     while not stack.is_empty():
-        postfix.append(stack.pop())
+        top_element = stack.pop()
+        if is_operand(top_element):
+            postfix.append(top_element)
     return ''.join(postfix)
-
 
 def is_operator(letter):
     if precedance.get(letter) is not None:
@@ -158,10 +93,7 @@ def is_operand(letter):
 def get_precedance(operator):
     # bracket will have precedance = 100 for time being
     # if more than 100 operators are added this should change
-    if operator == '(' or operator == ')':
-        return 100
-    else:
-        return precedance[operator]
+    return precedance[operator]
 
 def test_precedance():
     print get_precedance('+')
@@ -179,9 +111,15 @@ def test():
                  "ab+zx+*",
                  "at+bac++cd+^*"]
     for infix, postfix in zip(expressions, postfixes):
-        stack_size = len(infix)
-        assert infix_to_postfix(infix) == postfix
-        print infix, postfix
+        infix = "({})".format(infix)
+        try:
+            output = infix_to_postfix(infix)
+            assert output == postfix
+        except AssertionError:
+            print "Failed"
+        finally:
+            print infix, postfix, output
+            
 
 
 def process():
@@ -193,5 +131,5 @@ def process():
         count += 1
 
 
-# process()
-test()
+process()
+# test()
