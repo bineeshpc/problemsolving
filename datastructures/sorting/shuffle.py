bfc18ad6946fa7ae66165d4ed3d2ec9e3a4b9015
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2018-07-30 13:40:40 +0530

    my own shuffle as well

--- datastructures/sorting/shuffle.py
+++ datastructures/sorting/shuffle.py
@@ -1,7 +1,7 @@
 import quicksort
 import random
 
-def shuffle_sort(arr):
+def sort_shuffle(arr):
     """ 
     Generate a random number associated with every number and sort it
     This takes n log n time
@@ -25,4 +25,31 @@ def knuth_shuffle(arr):
         arr[position], arr[i] = arr[i], arr[position] # swaps 2 positions
         i += 1
     return arr
+
+def my_shuffle(arr):
+    """
+    Shuffle the array in O(n)
+    Generate two random numbers from left side and right side
+    choose one from these two to swap with the the ith position
+    i moving from 0 to length - 1
+    This one is complicated and I don't like this
+    """
+    
+    i = 0
+    length = len(arr) - 1
+    two_nums = [0, 0]
+    while i <= length:
+        if i > 0:
+            two_nums[0] = random.randint(0, i - 1)
+        else:
+            two_nums[0] = 0
+        if i < length:
+            two_nums[1] = random.randint(i + 1, length)
+        else:
+            two_nums[1] = length
+        num = random.choice(two_nums)
+        arr[i], arr[num] = arr[num], arr[i]
+        i += 1
+    return arr
+
 
