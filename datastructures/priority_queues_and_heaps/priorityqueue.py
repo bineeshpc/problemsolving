848137b40cd177abf70b99cc78bf3e217fc2fed8
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2018-07-30 14:04:19 +0530

    added couple of funcitons to priority queue like enqueue, dequeue

--- datastructures/priority_queues_and_heaps/priorityqueue.py
+++ datastructures/priority_queues_and_heaps/priorityqueue.py
@@ -2,5 +2,17 @@ import heap
 
 
 class PriorityQueue(heap.Heap):
-    pass
+    def enqueue(self.value):
+        self.insert(value)
+
+    def put(self.value):
+        self.enqueue(value)
+
+    def dequeue(self):
+        self.delete()
+
+    def get(self):
+        self.dequeue()
+
+
 
