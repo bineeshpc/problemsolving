5e4176a9f85baa0f7c0047b66e1ecccf4a21afa2
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2018-08-21 14:28:58 +0530

    write random file to disk

---
+++ utils/random_content_file.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import psutil
+import sys
+import random
+
+
+def parse_cmdline():
+    parser = argparse.ArgumentParser(description='Create files with random content to protect against personal data recovery')
+    parser.add_argument('directoryname', type=str,
+                        help='directoryname')
+    parser.add_argument('num_megabytes', type=int,
+                        help='number of megabytes to write as an integer')
+    args = parser.parse_args()
+    return args
+
+
+class DirectoryShouldBeNew(Exception):
+    pass
+
+
+def create_data(args):
+    directoryname = args.directoryname
+    if os.path.exists(directoryname):
+        raise DirectoryShouldBeNew
+    else:
+        os.mkdir(directoryname)
+    os.chdir(directoryname)
+    data = [chr(i) for i in range(256)]
+    
+    with open('filename', 'wb') as f:
+        size = 0
+        to_write = ''.join([random.choice(data) for i in range(1024 * 1024)])
+        while size < args.num_megabytes:
+            if random.random < .01:
+                to_write = ''.join([random.choice(data) for i in range(1024 * 1024)])
+            f.write(to_write)
+            size += 1
+    
+
+if __name__ == '__main__':
+    args = parse_cmdline()
+    create_data(args)
+    
