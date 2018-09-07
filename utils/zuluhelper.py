649e707d83d3854e133d1dd0d0fcfade6e721cc8
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2018-09-07 12:33:31 +0530

    mount multiple files one after the other

--- utils/zuluhelper.py
+++ utils/zuluhelper.py
@@ -7,7 +7,8 @@ import os
 def parse_cmd_line():
     parser = argparse.ArgumentParser(description='Helper for zulucrypt cli')
     parser.add_argument('--filename',
-                        help='path to a zulucrypt file or partition')
+                        help='path to a zulucrypt file or partition',
+                        action='append')
     parser.add_argument('--action', choices=['open', 'close'],
                         help='What action to perform open or close')
     args = parser.parse_args()
@@ -18,22 +19,26 @@ def execute(args):
     directory where the directory is actually mounted
     After we unmount we actually delete the directory
     """
-    cmds = {"open" : "sudo zuluCrypt-cli -o -d {path} -t vera -m {dirname} -M -p {password}",
+    cmds = {"open" : "sudo zuluCrypt-cli -o -d {path} -t vera -m {basename} -M -p {password}",
             "close": "sudo zuluCrypt-cli -q -d {path}"
             }
     dirname = "zulu"
-    cmd = cmds[args.action]
-    if args.action == 'open':
-        password = getpass.getpass(prompt='Enter the password :')
-        cmd = cmd.format(path=args.filename, dirname=dirname, password=password)
-        os.system('cd; mkdir -p {dirname}'.format(dirname=dirname))
-        os.system(cmd)
-        os.system('cd; cd {dirname}; ln -s /run/media/public/{dirname} {dirname}'.format(dirname=dirname))
-    
-    elif args.action == 'close':
-        cmd = cmd.format(path=args.filename)
-        os.system(cmd)
-        os.system('cd ; unlink {dirname}/{dirname} ; rmdir {dirname}'.format(dirname=dirname))
+    password = None
+    for filename in args.filename:
+        cmd = cmds[args.action]
+        basename = os.path.basename(filename)
+        if args.action == 'open':
+            if not password:
+                password = getpass.getpass(prompt='Enter the password :')
+            cmd = cmd.format(path=filename, basename=basename, password=password)
+            os.system('cd; mkdir -p {dirname}'.format(dirname=dirname))
+            os.system(cmd)
+            os.system('cd; cd {dirname}; ln -s /run/media/public/{basename} {basename}'.format(basename=basename, dirname=dirname))
+
+        elif args.action == 'close':
+            cmd = cmd.format(path=filename)
+            os.system(cmd)
+            os.system('cd ; unlink {dirname}/{basename} ; sleep 1; rmdir {dirname}'.format(dirname=dirname, basename=basename))
 
 def main():
     args = parse_cmd_line()
