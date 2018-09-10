7d1f668c9eb41fa7208c19f9ebdf12a0e4cf2b25
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2018-09-10 15:50:07 +0530

    added one more implementation of expression evaluation

--- datastructures/recursion_and_backtracking/digital_root.py
+++ datastructures/recursion_and_backtracking/digital_root.py
@@ -1,27 +1,25 @@
+import six
+
 def digital_root(num):
     """ digital root of a number is found by  
     the summing up all elements until we get a single
     digit number 
     1729   -> 19 -> 10 -> 1 (returns 1)
     """
+    
     def digital_sum(num):
-        # basecase number is 0
-        if num == 0:
+        if num < 10:
             return num
-        # recursive case
         else:
             last_digit = num % 10
-            remaining_number = num / 10
+            remaining_number = num // 10
             return last_digit + digital_sum(remaining_number)
-
-    # base case
+    
     if num < 10:
         return num
-    # recursive case
     else:
-        return digital_root(digital_sum(num))
+        digsum = digital_sum(num)
+        return digital_root(digsum)
 
 
-print digital_root(123456)
-        
-    
+six.print_(digital_root(10**20-1))
