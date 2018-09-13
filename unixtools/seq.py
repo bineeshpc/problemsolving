5621dfd42395902b735a27fa390665cb5ea068c9
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2018-09-13 07:39:36 +0530

    added unix like seq tool

---
+++ unixtools/seq.py
@@ -0,0 +1,24 @@
+import argparse
+import six
+
+def parse_cmdline():
+    parser = argparse.ArgumentParser(description='Unix like seq tool')
+    parser.add_argument('begin', 
+                        type=int,
+                        help='Print a sequence from the value of begin')
+    parser.add_argument('end', 
+                        type=int,
+                        help='Print a sequence till the value of end')
+    args = parser.parse_args()
+    return args
+
+def seq(begin, end):
+    for i in range(begin, end+1):
+        six.print_(i)
+
+def main():
+    args = parse_cmdline()
+    seq(args.begin, args.end)
+
+if __name__ == '__main__':  
+    main()
