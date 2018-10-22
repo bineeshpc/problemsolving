fc9c40a95e7d2842bed94d91e0e5b915fa519c57
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2018-10-22 16:35:10 +0530

    improved the design of get movie links, now it examines 5 pages instead of 1

--- utils/get_movie_links.py
+++ utils/get_movie_links.py
@@ -9,7 +9,6 @@ Get the magnetic links of movies to download later
 import os
 import re
 import argparse
-import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -24,9 +23,14 @@ import re
 import datetime
 import os
 import pickle
-
+import logging
 from pathlib import PureWindowsPath
 
+logger = logging.getLogger('get_movie_links')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('get_movie_links.log')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
 
 
 def parse_cmdline():
@@ -42,18 +46,74 @@ pattern_to_search = re.compile("""
 [,']  #patterns to remove
 """, re.VERBOSE)
 
-
-def get_magnetic_link(language):
-    url = "https://1337x.st/sort-search/{}/time/desc/1/".format(language)
-    req = requests.get(url, allow_redirects=True)
-    print(req.text)
+film_pattern = re.compile('>(.*)</a></td>')
 
 def getProfile():
     profile = webdriver.FirefoxProfile()
     profile.set_preference("browser.privatebrowsing.autostart", True)
     return profile
 
-class Downloader:
+
+def get_old_films():
+    filename = 'film_out.pickle'
+    with open(filename, 'rb') as f:
+        films = pickle.load(f)
+    return films
+
+
+class Page:
+    def __init__(self, language, page_number):
+        """ Initializes the language and the page number
+        """
+        self.__language = language
+        self.__page_number = page_number
+        self.__url = "https://1337x.st/sort-search/{}/time/desc/{}/".format(
+            self.__language,
+            self.__page_number)
+        self.__page_source = None
+        self.__films = []
+
+    def get_page(self, driver):
+        """ Downloads the page and saves it in a variable
+        """
+        driver.get(self.__url)
+        try:
+            element = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.ID, "autocomplete")))
+            logger.debug(self.__url + 'loaded')
+        except Exception as e:
+            logger.debug(self.__url + 'failed to load')
+        try:
+            driver.switch_to_alert().dismiss()
+        except Exception as e:
+            logger.debug(self.__url + 'did not have alert')
+            self.__page_source = driver.page_source
+            
+
+
+    def parse_films(self):    
+        potential_films = re.findall('.*coll-1 name.*', self.__page_source)
+        for potential_film in potential_films:
+            try:
+                film_name = film_pattern.search(potential_film).group(1).split('>')[-1]
+                film = Film(film_name, self.__language, self.__page_number)
+                self.__films.append(film)
+            except Exception as e:
+                logger.debug(self.__url + 'did not have films')
+                pass
+
+    def get_films(self):
+        return self.__films
+
+
+    def output_file(self):
+        output_file = "output_{}.html".format(language)
+        with open(output_file, 'w', encoding="utf-8") as f:
+            f.write(driver.page_source)
+        
+
+
+class Selenium:
 
     def __init__(self):
         self.setUp()
@@ -66,86 +126,6 @@ class Downloader:
             firefox_profile=getProfile()
          )
         self.verificationErrors = []
-        self.base_url = "https://1337x.st/sort-search/{}/time/desc/1/"
-        self.output_file = 'film_out.pickle'
-
-    def save_data(self, driver, language):
-        url = self.base_url.format(language)
-        driver.get(url)
-
-        try:
-            element = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.ID, "autocomplete"))
-            
-        )
-            print(element)
-        except Exception as e:
-            print(e)
-        try:
-            driver.switch_to_alert().dismiss()
-        except Exception as e:
-            print(e)
-        output_file = "output_{}.html".format(language)
-        with open(output_file, 'w', encoding="utf-8") as f:
-            f.write(driver.page_source)
-
-    def update_films(self, pattern, language, film_dict):
-        
-        output_file = "output_{}.html".format(language)
-        with open(output_file, encoding="utf-8") as f:
-            content = f.read()
-            potential_films = re.findall('.*coll-1 name.*', content)
-            for potential_film in potential_films:
-                try:
-                    film = pattern.search(potential_film).group(1).split('>')[-1]
-                    film_dict[language].append(film)
-                except Exception as e:
-                    print(e)
-                    pass
-        
-    def print_new_films(self, film_dict):
-        print("new films are:")    
-        with open(self.output_file, 'rb') as f:
-            film_dict_old = pickle.load(f)
-            for language in film_dict:
-                new_films = set(film_dict[language])
-                old_films = set(film_dict_old[language])
-                for film in (new_films - old_films):
-                    print(film)
-
-    def remove_files(self, languages):
-        for language in languages:
-            output_file = 'output_{}.html'.format(language)
-            os.remove(output_file)
-
-
-    def write_film_info_to_disk(self, film_dict):
-        text_output_file = 'film_output.txt'
-        old_text_output_file = 'film_output_1.txt'
-        os.remove(old_text_output_file)
-        os.rename(text_output_file, old_text_output_file)
-        with open(text_output_file, 'w') as f:
-            for language in film_dict:
-                f.write(language + '\n')
-                for film in film_dict[language]:
-                    f.write(film + '\n')
-
-        with open(self.output_file, 'wb') as f:
-            pickle.dump(film_dict, f, pickle.HIGHEST_PROTOCOL)
-    
-    def download(self):
-        driver = self.driver
-        languages = ["Malayalam", "Tamil", "bollywood"]
-        pattern = re.compile('>(.*)</a></td>')
-        film_dict = dict([(language, []) for language in languages])
-
-        for language in languages:
-            self.save_data(driver, language)
-            self.update_films(pattern, language, film_dict)
-        self.print_new_films(film_dict)
-        self.remove_files(languages)
-        self.write_film_info_to_disk(film_dict)
-        
     
     def is_element_present(self, how, what):
         try:
@@ -182,6 +162,93 @@ class Downloader:
     def __del__(self):
         self.tearDown()
 
+class MultiplePages:
+    """
+    This class is an agrregation of all Pages to download.
+    User class typically uses this class
+    """
+    def __init__(self, languages, begin, end):
+        """ Languages of the movies to download
+        Begin page number
+        End page number
+        Both inclusive
+        """
+        self.__languages = languages
+        self.__begin = begin
+        self.__end = end
+        self.__pages = []
+        for language in self.__languages:
+            for page_number in range(self.__begin, self.__end + 1):
+                self.__pages.append(Page(language, page_number))
+
+
+    def download_all(self, driver):
+        for page in self.__pages:
+            page.get_page(driver)
+    
+    def get_films(self):
+        films = []
+        for page in self.__pages:
+            page.parse_films()
+            films.extend(page.get_films())
+        return films
+
+class User:
+    """ This is the user class
+    User want to download some movies
+    User knows which urls to look for
+    And download
+    """
+    def __init__(self):
+        start_time = time.time()
+        sel_driver = Selenium()
+        languages = ["Malayalam", "Tamil", "bollywood"]
+        page_number_begin = 1
+        page_number_end = 5
+        self.output_file = 'film_out.pickle'
+        self.multiple_pages = MultiplePages(languages, page_number_begin, page_number_end)
+        self.multiple_pages.download_all(sel_driver.driver)
+        # self.print_films()
+        self.get_new_films()
+        self.save_pages()
+        end_time = time.time()
+        print('excuted in {} seconds'.format(end_time - start_time))
+
+    def print_films(self):
+        for film in self.multiple_pages.get_films():
+            print(film)
+
+    def save_pages(self):
+        with open(self.output_file, 'wb') as f:
+            pickle.dump(self.multiple_pages.get_films(), f, pickle.HIGHEST_PROTOCOL)
+
+    def get_new_films(self):
+        old_films = [film.get_name() for film in get_old_films()]
+        new_films_list = [film.get_name() for film in self.multiple_pages.get_films()]
+        logger.debug('old films are: ')
+        logger.debug(old_films)
+        logger.debug('new films are: ')
+        logger.debug(new_films_list)
+        new_films = set(new_films_list) - set(old_films)
+        for film in new_films:
+            print(film)
+
+class Film:
+    """ Stores info about film
+    """
+    def __init__(self, name, language, page_number):
+        self.__name = name
+        self.__language = language
+        self.__page_number = page_number
+
+    def __repr__(self):
+        return '({}, {}, {})'.format(self.__name, self.__language, self.__page_number)
+
+    def __eq__(self, other):
+        return self.__name == other.__name
+
+    def get_name(self):
+        return self.__name
+
 if __name__ == "__main__":
-    downloader = Downloader()
-    downloader.download()
+    User()
